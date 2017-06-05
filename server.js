789dd99a89d09c1780cf8780a3d560f1fc2cafb8
Boris Stephane NKUINJEU <nkuinjeustephane@gmail.com>
2017-06-05 21:43:44 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const hbs = require('hbs');
+const yargs = require('yargs');
+const fs = require('fs');
+
+const argv = yargs
+    .options({
+        n: {
+            demand: true,
+            string: true,
+            default: 'Stephane NKUINJEU',
+            alias: 'name',
+            describe: 'Name of user who launches app'
+        }
+    })
+    .help()
+    .argv;
+
+let app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((request, response, next) => {
+    let now = new Date().toString();
+    let log = `${now}: ${request.method} ${request.url}`;
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log(err);
+        }
+    });
+    next();
+});
+
+/*app.use((request, response, next) => {
+    response.render('maintenance.hbs');
+});*/
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (request, response) => {
+    response.render('home.hbs', {
+        pageTitle: 'About Page',
+        welcome: argv.name
+    });
+});
+
+app.get('/about', (request, response) => {
+    response.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+});
+
+app.get('/bad', (request, response) => {
+    response.send({
+        errorMessage: 'Unable to load the page'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on 3000 port');
+});
